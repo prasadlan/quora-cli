8aeaa86091246bffc4b02c72a47c63d2745038eb
prasadlan <prasad2311@gmail.com>
2017-09-28 13:43:31 -0400

    Revert "Changes to UI"

--- src/app/mock-questions.ts
+++
@@ -1,9 +0,0 @@
-import { Question } from './question';
-
-export const QUESTIONS: Question[] = [
-    {id: 1, question: 'Who is Tyrion Lannister?', answer:'Tyrion Lannister (also referred to as "the Imp" or "the Halfman") is a fictional character in A Song of Ice and Fire, a series of fantasy novels by American author George R. R. Martin and its television adaptation Game of Thrones.', user:'Anonymous', category_id: 1, date: 'Sep 24' },
-    {id: 2, question: 'Who is John Snow?', answer:'Jon Snow, born Aegon Targaryen, is the son of Lyanna Stark and Rhaegar Targaryen, the late Prince of Dragonstone.', user:'Anonymous', category_id: 2, date: 'Sep 24' },
-    {id: 3, question: 'Who is Daenerys Targaryen?', answer:'Daenerys is the only daughter and youngest child of King Aerys II Targaryen, the "Mad King", and his sister-wife, Rhaella.', user:'Anonymous', category_id: 1, date: 'Sep 25' },
-    {id: 4, question: 'Who is Arya Stark?', answer:'Arya is the younger daughter and third child of Lord Eddard Stark and his wife Lady Catelyn Stark.', user:'Anonymous', category_id: 1, date: 'Sep 26' },
-    {id: 5, question: 'Who is the Night King?', answer:'The Night King is the supreme leader and the first of the White Walkers, having existed since the age of the First Men. By extension, he is also the master of the Wights.', user:'Anonymous', category_id: 3, date: 'Sep 26' }
-];
--- src/app/question.ts
+++
@@ -1,8 +0,0 @@
-export class Question {
-    id: number;
-    question: string;
-    answer: string;
-    user: string;
-    category_id: number;
-    date: string;
-  }
