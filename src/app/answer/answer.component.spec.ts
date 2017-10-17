ffd7f76223e8998a44b29ffc5ef4c6852899db32
prasadlan <prasad2311@gmail.com>
2017-10-17 09:25:19 -0400

    component and service basic test fix

--- src/app/answer/answer.component.spec.ts
+++ src/app/answer/answer.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { NgModule, Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { MdButtonModule, MdListModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MatGridListModule, MatInputModule, MdTabsModule, MatExpansionModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from '../app.component';
 
+import { LoginComponent } from '../login/login.component';
+
+import { HomeComponent } from '../home/home.component';
 import { AnswerComponent } from './answer.component';
+import { HomeService } from '../home.service';
 
 describe('AnswerComponent', () => {
   let component: AnswerComponent;
@@ -8,7 +19,28 @@ describe('AnswerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnswerComponent ]
+      declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        AnswerComponent
+      ],
+      imports: [
+        RouterTestingModule,
+        HttpModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdListModule,
+        MdMenuModule,
+        MdCardModule,
+        MdToolbarModule,
+        MdIconModule,
+        MatInputModule,
+        MdTabsModule,
+        MatExpansionModule,
+      ],
+      providers: [ HomeService ],
     })
     .compileComponents();
   }));
