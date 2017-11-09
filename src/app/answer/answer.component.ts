21254f661a7bc0c42a7d672759fa2a5e8323143a
prasadlan <prasad2311@gmail.com>
2017-11-09 12:33:27 -0500

    option to submit answer for a question

--- src/app/answer/answer.component.ts
+++ src/app/answer/answer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home.service';
 import { Question } from '../question';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-answer',
@@ -10,9 +12,26 @@ import { Question } from '../question';
 export class AnswerComponent implements OnInit {
   questions: Question[];
   statusMessage: string;
+  answerform: FormGroup;
 
-  constructor(private _homeService:HomeService) {
+  constructor(private fb: FormBuilder, private router: Router, private _homeService:HomeService) {
+    this.answerform = fb.group({
+      'answer': ['']
+    });
+  }
 
+  toggleAnswerDialog(event): void {
+    let target = event.srcElement;
+    let parent = target.parentNode;
+    while(parent.nodeName != 'MD-CARD'){
+      parent = parent.parentNode;
+    }
+    let answerdialog = parent.lastElementChild;
+    if (answerdialog.style.display === "block") {
+      answerdialog.style.display = "none";
+    } else {
+      answerdialog.style.display = "block";
+    }
   }
 
   ngOnInit() {
@@ -21,6 +40,10 @@ export class AnswerComponent implements OnInit {
 
   getQuestions(): void {
     this.questions = this._homeService.getQuestions(); 
+  }
+
+  postAnswer(value): void {
+    
   }
 
 }
